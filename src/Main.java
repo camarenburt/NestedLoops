b6b212e9dd9b2a9c362f6e098656ba9970d23af7
621352 <camarenburt>
2023-11-06 09:35:37 -0500

    rtter

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        for (int i = 1; i <= 5; i++){
+            for(int j = 1; j<= i ; j++)
+                System.out.print("*");
+            System.out.println("");
+        }
+        System.out.println("\n");
+        for (int i = 5; i >= 1; i--){
+            for(int j = 1; j <= i ; j++)
+                System.out.print("*");
+            System.out.println("");
+        }
+        System.out.println("\n");
+        for (int i = 1; i <= 5; i++){
+            for(int j = 1; j<= 5; j++)
+                System.out.print("*");
+            System.out.println("");
+        }
     }
 }
+
